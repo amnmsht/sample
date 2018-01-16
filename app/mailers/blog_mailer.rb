6ae3c20bed085b0471dcb4b9b61e7381211475cd
masahito_amano <amano-m@nttpc.co.jp>
2018-01-16 07:11:27 +0000

    fix

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -1,7 +1,7 @@
 class BlogMailer < ApplicationMailer
     def blog_mail(blog)
- @blog = blog
+     @blog = blog
 
- mail to: "@blog.user.email", subject: "投稿内容の確認メール"
+ mail to: @blog.user.email, subject: "投稿内容の確認メール"
 end
 end
